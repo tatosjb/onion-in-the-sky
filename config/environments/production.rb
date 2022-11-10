43a6c880d68fc4fa07c1705bc5171b98b8972651
Altair J. Fernandes <con@tato.dev>
2022-11-10 13:57:23 -0300

    allow host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,6 +25,9 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
+  config.hosts << /[a-z0-9-.]+\.amazonlightsail\.com/
+  config.hosts << /[a-z0-9-.]+\.tato\.dev/
+
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
 
